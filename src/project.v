188462b91a5ce3d67eae9fe483e2801705246669
L0laapk3 <L0laapk3@gmail.com>
2026-04-13 01:57:59 +0200

    test 256 bit shift reg

--- src/project.v
+++ src/project.v
@@ -16,12 +16,20 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+	reg [255:0] r;
+	always @(posedge clk or negedge rst_n) begin
+		if (!rst_n) begin
+			r <= 0;
+		end else begin
+			r <= {r[254:0], ui_in[0]};
+		end
+	end
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[0] = r[255];
+  assign uo_out[7:1] = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
