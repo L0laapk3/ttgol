221dbb89826fc7cbb685e478442cc0db805767ee
L0laapk3 <L0laapk3@gmail.com>
2026-04-13 02:06:37 +0200

    fix errors?

--- src/project.v
+++ src/project.v
@@ -28,8 +28,10 @@ module tt_um_L0laapk3 (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out[0] = r[255];
   assign uo_out[7:1] = 0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, 1'b0};
+  wire _unused = &{ena, 1'b0, ui_in[7:1], uio_in};
 
 endmodule
